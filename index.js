0ba64265d33e8b0e6cfdb74c4b4cf829a1c1159e
Samad05imam <samad1419imam@gmail.com>
2025-07-12 02:06:27 +0530

    Update: Made changes to backend

--- index.js
+++ index.js
@@ -16,23 +16,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://jobjunction-samad.netlify.app"
+  "https://jobjunction-samad.netlify.app",
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 
 // Routes
 app.use("/api/v1/user", userRoute);
