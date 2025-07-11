b6f907a99608c1851fdf5f7fe92c6e494a42a273
Samad05imam <samad1419imam@gmail.com>
2025-07-12 01:56:56 +0530

    Update: Made changes to backend

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOption = {
-    origin: "http://localhost:5173",
-    credentials: true,
+  origin: [
+    "http://localhost:5173", // for development
+    "https://jobjunction-samad.netlify.app/" // replace after Netlify deploy
+  ], credentials: true,
 };
 app.use(cors(corsOption));
 
